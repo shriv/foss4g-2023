f6d8fb60e9a38460acc3f018ba332b9292578e02
Shrividya Ravi <shrividyaravi@Shrividyas-MacBook-Pro.local>
2023-10-05 10:12:44 +1300

    Adding a new chapter on urban views

---
+++ R/street_view.R
@@ -0,0 +1,28 @@
+library(config)
+library(googleway)
+
+get_street_view <- function(latitude, longitude, heading, filepath){
+  # Get API key from config file
+  key <- config::get("key")
+  
+  # Get streetview images from Google
+  # Latitude and longitude can be obtained from Google URL
+  # Use the compass mark to estimate a heading value
+  # A heading of 0 / 360 = North; 180 = South; 90 = East; 270 = West
+  # FOV notes the zoom level
+  # TODO Convert to function
+  if (!file.exists(filepath)){
+    url <- google_streetview(location = c(latitude,longitude),
+                             size = c(400,400),
+                             panorama_id = NULL,
+                             output = "html",
+                             heading = heading,
+                             fov =90,
+                             pitch = 5,
+                             response_check = FALSE,
+                             key = key)
+    
+    download.file(url, filepath, model="wb")  
+  }
+  
+}
